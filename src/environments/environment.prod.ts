ef49f84f22a470e76478e68f0ab9e45b7871c161
DaarcyDev <liam_alvarez@hotmail.com>
2023-12-05 17:28:18 -0600

    ionic tutorial

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCfhGUb68yfUjsKMk0dfdq3_nUcvJc1dcE",
+    authDomain: "product-admin-ionic-91021.firebaseapp.com",
+    projectId: "product-admin-ionic-91021",
+    storageBucket: "product-admin-ionic-91021.appspot.com",
+    messagingSenderId: "308143617717",
+    appId: "1:308143617717:web:58f7ed65c8fec2f8f9f0fc"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyCfhGUb68yfUjsKMk0dfdq3_nUcvJc1dcE",
+    authDomain: "product-admin-ionic-91021.firebaseapp.com",
+    projectId: "product-admin-ionic-91021",
+    storageBucket: "product-admin-ionic-91021.appspot.com",
+    messagingSenderId: "308143617717",
+    appId: "1:308143617717:web:58f7ed65c8fec2f8f9f0fc"
+  }
 };
 
 /*
